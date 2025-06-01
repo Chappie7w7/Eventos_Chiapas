b24c884b3d62522c8d81b7ba11e42f939fa0b0da
JaniletPP <166181235+JaniletPP@users.noreply.github.com>
2025-06-01 02:13:11 -0600

    Agregar validación básica en el formulario con JavaScript

---
+++ js/contacto.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    const nombre = document.getElementById("nombre").value.trim();
+    const correo = document.getElementById("correo").value.trim();
+    const mensaje = document.getElementById("mensaje").value.trim();
+
+    if (!nombre || !correo || !mensaje) {
+      alert("Por favor, llena todos los campos.");
+      e.preventDefault(); // Evita el envío
+    }
+  });
+});
